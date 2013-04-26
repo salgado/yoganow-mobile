ce988b4f54f2401f012fe92ca1381c2f8951fccc
Steven Zeiler <zeiler.steven@gmail.com>
2013-04-26 14:38:50 -0700

    make buttons switch screens.

--- my.js
+++ my.js
@@ -1,20 +1,34 @@
 $(function(){
+	// Prevents all anchor click handling
+	$.mobile.linkBindingEnabled = false;
+	// Disabling this will prevent jQuery Mobile from handling hash changes
+	$.mobile.hashListeningEnabled = false;
+
+
 	url = "http://yoganow-api.herokuapp.com/yoga_studios.json?location=301%20Bowie%20Austin"
+	$.mobile.loading( 'show');
 	$.getJSON(url,function(data){
 		window.studios = data.yoga_studios
-		updateList(window.studios);		
+		setTimeout(function(){
+			updateList(window.studios);
+			$.mobile.loading( 'hide');
+		},0);		
 	});
 
 	function updateList(studios){
-		var template = _.template($("#listItemTemplate").html());
-		var $ul = $("<ul/>");
+		var listItemTemplate = _.template($("#listItemTemplate").html());		
+		var listTemplate = _.template($("#listTemplate").html());	
+
+		$ul = $(listTemplate({}));	
+			
+		console.log($ul);
 		
 		$.each(studios, function(i, studio){ 
 			studio.distance = parseFloat(studio.distance).toFixed(2);
-			$ul.append(template(studio));
+			$ul.append(listItemTemplate(studio));
 		});
-		$(".ui-listview").html($ul.html());
-		$(".ui-listview").listview();
+		$("[data-role=content]").html($ul);
+		$ul.listview();
 	}
 
 	function filterStudios(search, studios){
@@ -26,6 +40,7 @@ $(function(){
 
 	$(".ui-input-text").on("keyup", function(){
 		var value = $(".ui-input-text").val();
+		console.log(value);
 		filterStudios(value, window.studios);
 	});
 
@@ -34,4 +49,50 @@ $(function(){
 	});
 
 	$('[data-position=fixed]').fixedtoolbar({ tapToggle:false});
+
+	function showMap() {
+		$(".ui-listview").hide();
+		$("#content").html($("<div id='map-canvas'/>"));
+
+		var mapOptions = {
+		    zoom: 8,
+		    center: new google.maps.LatLng(-34.397, 150.644),
+		    mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		map = new google.maps.Map(document.getElementById('map-canvas'),
+		      mapOptions);
+
+		google.maps.event.trigger(map,'resize');		
+	}
+
+	var App = Backbone.Router.extend({
+
+		routes: {
+		  "map": "map",   
+		  "list": "list",  
+		},
+
+		map: function() {
+		  showMap();
+		},
+
+		list: function() {		
+		  updateList(window.studios);
+		  $(".ui-listview").show();
+		}
+
+	});
+
+	window.app = new App();
+
+	Backbone.history.start()
+
+	$("#map").on('click',function(){
+		app.navigate("/map", true);
+	});
+
+	$("#list").on('click',function(){
+		app.navigate("/list", true);
+	});
 });
