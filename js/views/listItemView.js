5f47001491647a51724b5e9f6b36f5436dae4454
Steven Zeiler <zeiler.steven@gmail.com>
2013-05-10 11:04:12 -0500

    delete old files.

--- js/views/listItemView.js
+++
@@ -1,7 +0,0 @@
-ListItemView = Backbone.View.extend({		
-	template: _.template($("#listItemTemplate").html()),
-	render: function(){
-		this.model.attributes
-		return this.template(this.model.attributes);
-	}
-});
